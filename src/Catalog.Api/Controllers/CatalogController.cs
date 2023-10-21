869b7406fb8010015723b18713c50d7852ad0fd8
bertalankiss <Bertalan_Kiss@epam.com>
2023-10-21 15:18:19 +0200

    Initial API

---
+++ src/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CatalogController : ControllerBase
+{
+    private readonly ILogger<CatalogController> _logger;
+
+    public CatalogController(ILogger<CatalogController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetCatalog")]
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+}
+
