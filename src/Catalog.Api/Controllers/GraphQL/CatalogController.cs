14d0e3c869e2c115d4a35c3036cbff4baee4a31f
bertalankiss <Bertalan_Kiss@epam.com>
2023-12-07 22:35:38 +0100

    GraphQL integration added

---
+++ src/Catalog.Api/Controllers/GraphQL/CatalogController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GraphQL.AspNet.Attributes;
+using System.Drawing;
+using GraphQL.AspNet.Controllers;
+using Catalog.Domain.Entities;
+using Catalog.Application.Services;
+using Catalog.Api.Requests;
+using Catalog.Api.Mappers;
+
+namespace Catalog.Api.Controllers.GraphQL
+{
+	public class CatalogController : GraphController
+    {
+        private readonly ICategoryService categoryService;
+        private readonly IItemService itemService;
+
+        public CatalogController(ICategoryService categoryService, IItemService itemService)
+        {
+            this.categoryService = categoryService;
+            this.itemService = itemService;
+        }
+
+        [QueryRoot("categories")]
+        public async Task<IEnumerable<Category>> ListCategories()
+        {
+            return await categoryService.List();
+        }
+
+        [QueryRoot("items")]
+        public async Task<IEnumerable<Item>> ListItems(int categoryId, int pageSize, int page)
+        {
+            return await itemService.List(categoryId, pageSize, page);
+        }
+
+        [MutationRoot("addCategory")]
+        public async Task<int?> AddCategory(CategoryRequest request)
+        {
+            if (!ModelState.IsValid)
+                return null;
+
+            var category = CategoryMapper.Map(request);
+            return await categoryService.Add(category);
+        }
+    }
+}
+
--- src/Catalog.Api/Requests/CategoryRequest.cs
+++ src/Catalog.Api/Requests/CategoryRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Catalog.Api.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Api.Requests
 {
     public class CategoryRequest
     {
+        [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
         public string ImageUrl { get; set; }
         public int? ParentCategoryId { get; set; }
